682f020c15461b112605db952fd5743a53ea31e5
Isa147R <131714039+Isa147R@users.noreply.github.com>
2023-05-19 14:30:03 -0500

    Carga_Datos.sql

--- Carga_Datos.sql
+++ Carga_Datos.sql
@@ -36,8 +36,21 @@ Select * from Mes;
 COPY Tipo_emisor(nombre,codigo)  FROM '/Users/isabela/emisor.csv' WITH DELIMITER ';' CSV HEADER;
 Select * from Tipo_emisor;
 
-COPY Linea(nombre,id_Zona)  FROM '/Users/isabela/Linea1.csv' WITH DELIMITER ';' CSV HEADER;
-Select * from Linea;
+
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(11) Zona K Calle 26', 6);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(12) Zona L Carrera 10', 9);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(30) Zona G NQS Sur', 2);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(31) Zona F Av. Américas', 1);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(32) Zona C Av. Suba', 10);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(33) Zona B AutoNorte', 18);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(34) Zona H Caracas Sur', 17);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(35) Zona D Calle 80', 4);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(36) Zona A Caracas', 8);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(37) Zona J Eje Ambiental', 8);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(38) Zona E NQS Central', 6);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(39) Zona F Calle 13', 8);
+INSERT INTO Linea(nombre, id_Zona) VALUES ('(40) Zona T Ciudad Bolívar', 4);
+SELECT * FROM Linea;
 
 COPY Estacion(nombre,id_Linea)  FROM '/Users/isabela/Estaciones1.csv' WITH DELIMITER ';' CSV HEADER;
 Select * from Estacion;
@@ -51,14 +64,14 @@ Select * from Tarjeta;
 COPY Transaccion(saldo_antes,saldo_despues,id_Tarjeta,id_Dispositivo)  FROM '/Users/isabela/Transaccion1.csv' WITH DELIMITER ';' CSV HEADER;
 Select * from Transaccion;
 
+COPY Pagar (id_Estacion,id_Transaccion)  FROM '/Users/isabela/Pagar2.csv' WITH DELIMITER ';' CSV HEADER;
+Select * from Pagar;
+
 COPY Servicio(id_Zona,id_Tipo_servicio,id_Tipo_vehiculo,id_Accesibilidad )  FROM '/Users/isabela/Servicio1.csv' WITH DELIMITER ';' CSV HEADER;
 Select * from Servicio;
 
 COPY Vehiculo(etiqueta,matricula ,id_Concesion,modelo,id_Marca,id_Nivel_emision ,id_Zona)  FROM '/Users/isabela/Vehiculo1.csv' WITH DELIMITER ';' CSV HEADER;
 Select * from Vehiculo;
-
-COPY Pagar (id_Estacion,id_Transaccion)  FROM '/Users/isabela/Pagar2.csv' WITH DELIMITER ';' CSV HEADER;
-Select * from Pagar;
 
 
 
