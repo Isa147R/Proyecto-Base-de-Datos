43e245fa8a1abbc0f9b7bd6e898cb8b98a2b8c6e
Isa147R <131714039+Isa147R@users.noreply.github.com>
2023-05-20 15:08:30 -0500

    Analisis.py

--- Analisis.py
+++ Analisis.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from Connection1 import Connection1
@@ -10,19 +11,21 @@ external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
-# Usuarios por zona
 
+# ------------------------------------------------- Graficos Barras -------------------------------------------------
+
+# Usuarios por zona
 con = Connection1()
 con.openConnection1()
 query = pd.read_sql_query(sql.usuarios_por_zona(),con.connection)
 con.closeConnection1()
 dfCases = pd.DataFrame(query,columns=["zona","personas_utilizan_transporte"])
-figBarCases = px.bar(dfCases.head(20),x="zona", y="personas_utilizan_transporte",color_discrete_sequence=['#0B1F2A'])
+figBarCases = px.bar(dfCases.head(20),x="zona", y="personas_utilizan_transporte",color_discrete_sequence=['#395A4B'])
 
-# Configurar la fuente en los gráficos
+# Configurar la fuente en los gráficos Usuarios por zona
 figBarCases.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='#11414E',
+    plot_bgcolor='#5A8573',
     font=dict(
         family="Arial, sans-serif",
         size=14,
@@ -31,17 +34,17 @@ figBarCases.update_layout(
 )
 
 # Usuarios por estacion
-
 con = Connection1()
 con.openConnection1()
 query = pd.read_sql_query(sql.usuarios_por_estacion(),con.connection)
 con.closeConnection1()
 dfCases = pd.DataFrame(query,columns=["estacion","personas_utilizan_transporte"])
-figBarCases1 = px.bar(dfCases.head(20),x="estacion", y="personas_utilizan_transporte",color_discrete_sequence=['#0B1F2A'])
+figBarCases1 = px.bar(dfCases.head(20),x="estacion", y="personas_utilizan_transporte",color_discrete_sequence=['#395A4B'])
 
+# Configurar la fuente de los graficos Usuarios por estacion
 figBarCases1.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='#11414E',
+    plot_bgcolor='#5A8573',
     font=dict(
         family="Arial, sans-serif",
         size=14,
@@ -50,18 +53,18 @@ figBarCases1.update_layout(
 )
 
 
-# usuarios por tipo_perfil
-
+# Usuarios por tipo_perfil
 con = Connection1()
 con.openConnection1()
 query = pd.read_sql_query(sql.tipo_perfil_mas_utilizado(),con.connection)
 con.closeConnection1()
 dfCases = pd.DataFrame(query,columns=["perfil","total_utilizado"])
-figBarCases2 = px.bar(dfCases.head(20),x="perfil", y="total_utilizado",color_discrete_sequence=['#0B1F2A'])
+figBarCases2 = px.bar(dfCases.head(20),x="perfil", y="total_utilizado",color_discrete_sequence=['#395A4B'])
 
+#Configurar la fuente de los graficos Usuarios por tipo de perfil 
 figBarCases2.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='#11414E',
+    plot_bgcolor='#5A8573',
     font=dict(
         family="Arial, sans-serif",
         size=14,
@@ -70,17 +73,17 @@ figBarCases2.update_layout(
 )
 
 # salidas por estaciones
-
 con = Connection1()
 con.openConnection1()
 query = pd.read_sql_query(sql.salidas_por_estacion(),con.connection)
 con.closeConnection1()
 dfCases = pd.DataFrame(query,columns=["nombre","total_salidas"])
-figBarCases3 = px.bar(dfCases.head(20),x="nombre", y="total_salidas",color_discrete_sequence=['#0B1F2A'])
+figBarCases3 = px.bar(dfCases.head(20),x="nombre", y="total_salidas",color_discrete_sequence=['#395A4B'])
 
+#Configurar la fuente de los graficos Salidas por estaciones
 figBarCases3.update_layout(
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='#11414E',
+    plot_bgcolor='#5A8573',
     font=dict(
         family="Arial, sans-serif",
         size=14,
@@ -88,13 +91,96 @@ figBarCases3.update_layout(
     )
 )
 
-#Layout
+
+
+# ------------------------------------------------- Graficos pie -------------------------------------------------
+
+con = Connection1()
+con.openConnection1()
+query = pd.read_sql_query(sql.generar_grafico_pie_zonas(),con.connection)
+con.closeConnection1()
+dfCases = pd.DataFrame(query,columns=["cantidad","zona"])
+figPie1 = px.pie(dfCases, values='cantidad', names='zona', color_discrete_sequence=['#FFA552'])
+
+# Configurar la fuente de los gráficos Salidas por zonas
+figPie1.update_layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='#FDFFFC',
+    font=dict(
+        family="Arial, sans-serif",
+        size=14,
+        color="#FFFFFC"
+    )
+)
+
+# ------------------------------------------------- Tabs Graficos de Pie -------------------------------------------------
+tab2_content = dbc.Card(
+        dbc.CardBody([
+            html.H3("Cantidad de registros por zona"),
+            dcc.Graph(
+                id='piechart',
+                figure=figPie1,
+                style={'width': '100%'}
+            )
+        ]),
+        className="mt-4",
+        style={'background-color': '#234D60'}
+    )
+
+
+# ------------------------------------------------- Grafico Lineas -------------------------------------------------                            
+con = Connection1()
+con.openConnection1()
+query = pd.read_sql_query(sql.salidas_hora_pico_3_8(), con.connection)
+con.closeConnection1()
+dfCases = pd.DataFrame(query, columns=["id_Estacion", "total_salidas"])
+figLine = px.line(dfCases,x="id_Estacion", y="total_salidas",  color_discrete_sequence=['#0B1F2A'])
+
+#Configurar la fuente de los graficos Salidas por estaciones
+figLine.update_layout(
+    paper_bgcolor='rgba(0, 0, 0, 0)',
+    plot_bgcolor='#5A8573',
+    font=dict(
+        family="Arial, sans-serif",
+        size=14,
+        color="#FFFFFC"
+    )
+)
+
+
+# ------------------------------------------------- Tabs Grafico Lineas -------------------------------------------------                            
+
+tab4_content = dbc.Card(
+    dbc.CardBody([
+        html.H3("Gráfico de Linea"),
+        dcc.Graph(
+            id='LineGraph',
+            figure=figLine,
+            style={'width': '100%'}
+        )
+    ]),
+    className="mt-4",
+    style={'background-color': '#314913'}
+)
+
+
+
+
+
+
+# ------------------------------------------------- Layout-------------------------------------------------
 
 app.layout = html.Div(children=[
-    
+    # ------------------------------------------------- Encabezado -------------------------------------------------
     html.H1(children='Analisis Sistema de Transporte Masivo Bogota', style={'margin-left': '35px'}),
     html.H6(children='Isabela Ruiz', style={'margin-left': '35px'}),
     html.Div(style={'height': '50px'}),
+    # ------------------------------------------------- Tabs Pie-------------------------------------------------
+    dbc.Tabs([
+        dbc.Tab(tab2_content, label="Servicios"),
+    ]),
+    # ------------------------------------------------- Graficos de barras -------------------------------------------------
+    # Usuarios por zona
     html.Div(
             className='container',
             style={'display': 'flex', 'align-items': 'center', 'margin-bottom': '20px','font-family': 'Arial, sans-serif'},
@@ -135,6 +221,7 @@ app.layout = html.Div(children=[
                 )
             ]
         ),
+    # Usuarios por estacion                            
     html.Div(style={'height': '50px'}),                            
     html.Div(
             className='container',
@@ -174,6 +261,7 @@ app.layout = html.Div(children=[
                 )
             ]
         ),
+    # Usuarios por perfil                            
     html.Div(style={'height': '50px'}),
     html.Div(
             className='container',
@@ -211,6 +299,7 @@ app.layout = html.Div(children=[
                 )
             ]
         ),
+    # Salidas por estacion                            
     html.Div(style={'height': '50px'}),
     html.Div(
             className='container',
@@ -236,7 +325,7 @@ app.layout = html.Div(children=[
                 html.Div(
                     style={'margin-left': '20px', 'width': '50%', 'white-space': 'pre-wrap'},
                     children=[
-                        html.H3(" ¿Que estaciones registraron mas salidas? "),
+                        html.H3(" ¿Ques estaciones registraron mas salidas? "),
                         html.Div(style={'height': '20px'}),
                         html.H6("""
                         SELECT Estacion.nombre, SUM(Salidas.total) AS total_salidas
@@ -250,7 +339,11 @@ app.layout = html.Div(children=[
                     ]
                 )
             ]
-        ),                            
+        ),
+    # ------------------------------------------------- Tabs Grafico Lineas -------------------------------------------------                            
+    dbc.Tabs([
+        dbc.Tab(tab4_content, label="Graficos de Linea"),
+    ]),
     html.Label('Color Picker'),
     dcc.Input(
         id='our-color-picker',
@@ -259,10 +352,14 @@ app.layout = html.Div(children=[
         style={'width': '80px'}
     ),
 
-], style={'background': '#153243', 'color': '#FFFFFC',})
+], style={'background': '#366B84', 'color': '#FFFFFC',})
+
+
 
 
 if __name__ == '__main__':
     app.run_server(debug = True)
+    
+    
     
     
